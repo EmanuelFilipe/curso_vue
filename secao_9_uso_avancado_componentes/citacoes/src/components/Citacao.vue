b8910f3c76686cb79fa7af6f2d8fd79d6969bf1e
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-06-22 23:16:38 -0300

    Seção 9: Uso Avançado de Componentes

---
+++ secao_9_uso_avancado_componentes/citacoes/src/components/Citacao.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+        <!-- slot nomeado -->
+        <slot name="fonte"></slot>
+        <slot name="texto"></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //listaCitacoes: String,
+  },
+  
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+
+h1 {
+  color: red !important;
+}
+</style>
