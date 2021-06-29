d1f04935dee9b09117b5d2c9f9f0f00ad77f1ab5
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-06-29 18:44:39 -0300

    Seção 11: Manipulando Entrada de Usuário com Formulários

---
+++ secao_11_inputs-formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="teste1">
+        <Rotulo nome="Nome">
+            <input type="text" @input="alterarNome" :value="value.nome">
+        </Rotulo>
+    </div>
+    <!-- <div class="teste2">
+        <Rotulo nome="Sobrenome">
+            <input type="text" :value="value.sobrenome">
+        </Rotulo>
+    </div> -->
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+
+export default {
+  components: { Rotulo },
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    alterarNome(event) {
+      this.$emit("input", { nome: event.target.value });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.nomeCompleto {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 15px 0px;
+  font-size: 1.4rem;
+}
+
+.elemento {
+  display: flex;
+  align-self: stretch;
+  margin: 10px 0px;
+}
+
+input,
+textarea,
+select {
+  flex: 1;
+  font-size: 1.4rem;
+  outline: none;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 5px 10px;
+}
+
+input:focus {
+  border: 1px solid #bbb;
+}
+</style>
