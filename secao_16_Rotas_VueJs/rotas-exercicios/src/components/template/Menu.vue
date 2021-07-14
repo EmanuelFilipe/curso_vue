374711a362d4112208ab49408630b9fc23d422db
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-07-14 07:12:13 -0300

    Seção 16: Rotas

---
+++ secao_16_Rotas_VueJs/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="menu">
+        <ul>
+            <router-link to="/" tag="li" active-class="active" exact>
+                Início
+            </router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                Usuário</router-link>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ secao_16_Rotas_VueJs/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuário Detalhe</h3>
+        <p><strong>Código: </strong>{{id}}</p>
+        <!-- <router-link tag="button" primario :to="`/usuario/${id}/editar`">
+          Editar
+        </router-link> -->
+        <router-link tag="button" primario :to="{ name: 'editarUsuario', params: { id },
+          query: { completo: false, lingua: 'pt'}, hash: '#rodape' }">
+          Editar
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+  // watch: {
+  //   // to é o atributo com o valor novo -- from é o atributo com valor antigo
+  //   $route(to, from) {
+  //     this.id = to.params.id;
+  //   },
+  // },
+  props: ["id"],
+  beforeRouteEnter(to, from, next) {
+    console.log("dentro do componente -> usuario detalhe");
+    console.log("to:", to);
+    console.log("from:", from);
+    next();
+  },
+};
+</script>
+
+<style>
+</style>
