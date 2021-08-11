1817763e94247d6f9b207d24ca52508f732aa6e2
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-08-11 16:27:38 -0300

    Seção 20: Bonus: Projeto #04 - Projeto Calculadora

---
+++ secao_20/calculator/src/main/Calculator.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="calculator">
+    <Display :value="displayValue" />
+    <Button label="AC" triple @onCalcButtonClick="clearMemory" />
+    <Button label="/" operation @onCalcButtonClick="setOperation" />
+    <Button label="7" @onCalcButtonClick="addDigit" />
+    <Button label="8" @onCalcButtonClick="addDigit" />
+    <Button label="9" @onCalcButtonClick="addDigit" />
+    <Button label="*" operation @onCalcButtonClick="setOperation" />
+    <Button label="4" @onCalcButtonClick="addDigit" />
+    <Button label="5" @onCalcButtonClick="addDigit" />
+    <Button label="6" @onCalcButtonClick="addDigit" />
+    <Button label="-" operation @onCalcButtonClick="setOperation" />
+    <Button label="1" @onCalcButtonClick="addDigit" />
+    <Button label="2" @onCalcButtonClick="addDigit" />
+    <Button label="3" @onCalcButtonClick="addDigit" />
+    <Button label="+" operation @onCalcButtonClick="setOperation" />
+    <Button label="0" double @onCalcButtonClick="addDigit" />
+    <Button label="." @onCalcButtonClick="addDigit" />
+    <Button label="=" operation />
+  </div>
+</template>
+
+<script>
+import Display from "../components/Display.vue";
+import Button from "../components/Button.vue";
+
+export default {
+  components: { Display, Button },
+  data() {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0, 0],
+      current: 0,
+    };
+  },
+  methods: {
+    clearMemory() {
+      //this.$options.data() | estado inicial do objeto
+      Object.assign(this.$data, this.$options.data());
+    },
+    setOperation(operation) {
+      if (this.current === 0) {
+        this.operation = operation;
+        this.current = 1;
+        this.clearDisplay = true;
+      } else {
+        let equals = operation === "=";
+        let currentOperation = this.operation;
+
+        try {
+          this.values[0] = eval(
+            `${this.values[0]} ${currentOperation} ${this.values[1]}`
+          );
+        } catch (e) {
+          this.$emmit("onError", e);
+        }
+
+        this.values[1] = 0;
+        this.displayValue = this.values[0];
+        this.operation = equals ? null : operation;
+        this.current = equals ? 0 : 1;
+        this.clearDisplay = !equals;
+      }
+    },
+    addDigit(n) {
+      if (n === "." && this.displayValue.includes(".")) return;
+
+      let clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      let currentValue = clearDisplay ? "" : this.displayValue;
+      let displayValue = currentValue + n;
+
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
+      this.values[this.current] = displayValue;
+    },
+  },
+};
+</script>
+
+<style>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
